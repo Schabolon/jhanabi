fa84a0407d9acf50951b45bf7f8e86b583075dbb
Patrick Bui <Patrick.Bui@faktorzehn.de>
2017-09-01 16:47:56 +0200

    created server class

---
+++ src/hanabi/server/Server.java
@@ -0,0 +1,14 @@
+package hanabi.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main (String[] args) {
+		
+
+}
+}
